5d45abe3db5eecff06f0e35050100ef56cf30bf5
rupesh0099 <rupeshdighe0120@gmail.com>
2026-05-06 15:48:49 +0530

    asg added

---
+++ Assignment/02-Singly Linked List/25-Assignment/q25.c
@@ -0,0 +1,79 @@
+/*"create a linnked list and print the number of nodes the list have. (size of list)
+I/P  ---->   |_1_|_AA_|_100_| --> |_2_|_BB_|_200_| --> |_3_|_CC_|_300_| --> |_4_|_DD_|_NULL_| 
+O/P ---->  4"*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct singly
+{
+    int Number;
+    char Name[10];
+    int standard;
+    struct singly *next;
+} node;
+node *create();
+node *countnode(node *);
+void freeList(node *);
+int main()
+{
+    node *head = NULL;
+    head = create();
+    head = countnode(head);
+    freeList(head);
+    return 0;
+}
+node *create()
+{
+    node *head = NULL, *newNode, *lastNode = NULL;
+    int choice;
+    do
+    {
+        newNode = (node *)malloc(sizeof(node));
+        if (newNode == NULL)
+        {
+            printf("Memory allocation failed!\n");
+            return head;
+        }
+        printf("\nEnter the number:\n");
+        scanf("%d", &newNode->Number);
+        getchar();
+        printf("Enter name: ");
+        fgets(newNode->Name, sizeof(newNode->Name), stdin);
+        newNode->Name[strcspn(newNode->Name, "\n")] = '\0';
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            lastNode->next = newNode;
+        }
+        lastNode = newNode;
+        printf("do you want to enter more records?(1 for yes/ 0 for NO):");
+        scanf("%d", &choice);
+    } while (choice == 1);
+    return head;
+}
+node* countnode(node* head) {
+    int count = 0;
+    node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    printf("\nNumber of nodes in the list: %d\n", count);
+    return head;
+}
+
+void freeList(node *head)
+{
+    node *temp;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
